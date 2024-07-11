456d123e36b070eda287c9d2e0f77dd0a83f2263
Otavio Lhamas <otavio.m.lhamas@gmail.com>
2024-07-11 12:03:10 -0300

    background opacity

--- config/appearance.lua
+++ config/appearance.lua
@@ -13,26 +13,23 @@ local font_size = 12
 --#endregion
 -----------------------------------
 
-local inactive_pane_hsb = {
-    saturation = 0.9,
-    brightness = 0.65,
-}
-
--- window
-local window_padding = {
-    left = 5,
-    right = 5,
-    top = 5,
-    bottom = 5,
-}
-
-local window_decorations = 'INTEGRATED_BUTTONS|RESIZE'
-
 return {
     color_scheme = color_scheme,
     font = font,
     font_size = font_size,
-    inactive_pane_hsb = inactive_pane_hsb,
-    window_padding = window_padding,
-    window_decorations = window_decorations
+
+    inactive_pane_hsb = {
+        saturation = 0.9,
+        brightness = 0.65,
+    },
+
+    -- window
+    window_background_opacity = 0.9,
+    window_decorations = 'INTEGRATED_BUTTONS|RESIZE',
+    window_padding = {
+        left = 5,
+        right = 5,
+        top = 5,
+        bottom = 5,
+    },
 }
